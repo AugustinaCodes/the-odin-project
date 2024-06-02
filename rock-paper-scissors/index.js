d65fd98d799c4f5e0043e10562f5b382f3dfdd12
August <76185140+Augustina1995@users.noreply.github.com>
2024-06-02 20:30:23 +0300

    Wrote the logic to get the computer choice for the game

--- rock-paper-scissors/index.js
+++ rock-paper-scissors/index.js
@@ -1,1 +1,16 @@
-console.log("Hello world");
+// writing code for the computer choice, so it randomly returns one of the following string values "rock", "paper", "scissors".
+
+function getComputerChoice(){
+    // first we need to put the choices in a variable, to store them somewhere. So put these strings in an array
+    const choices = ["rock", "paper", "scissors"];
+
+    // Math.random returns a random number that's >= 0 and <1. If we want to get a number between 0 and 10, we need to multiply the answe by 10, eg Math.Random() * 10
+    // I'd also need to use Math.floor as it rounds a number down to an integer
+    // So I put choices[code]. The code inside calculates a number randomly, so the result displays always a random string from an array. 
+
+    const result = choices[Math.floor(Math.random() * choices.length)];
+
+    return result;
+}
+
+console.log(getComputerChoice());
