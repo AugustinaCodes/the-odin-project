ba024335fe3d21bf4f787087c7e73ec932c683e9
August <76185140+Augustina1995@users.noreply.github.com>
2025-06-10 12:29:11 +0300

    practice

---
+++ foundations/javascript_basics/variables_and_operators/practice.js
@@ -0,0 +1,30 @@
+let admin
+let name = "John"
+
+admin = name
+
+console.log(admin);
+
+// comparing objects in JavaScript always returns false
+
+let x = new Number(123)
+let y = new Number (456)
+
+console.log(x === y);
+
+// The Number() function converts anything passed to it into anumber if it can
+
+const myString = "123"
+const myNum = Number(myString)
+
+console.log(myNum);
+
+// And String() function converts its argument to a string
+
+const myNum2 = 123
+const myString2 = String(myNum2)
+
+console.log(myString2);
+
+
+
