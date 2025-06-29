8f0ef7e9bb6a85727ed861def67ba98742aa92f8
August <76185140+Augustina1995@users.noreply.github.com>
2025-06-29 18:15:00 +0300

    DOM practice with MDN

--- foundations/javascript_basics/dom_and_events/script.js
+++ foundations/javascript_basics/dom_and_events/script.js
@@ -1,26 +1,41 @@
-const paragraph = document.createElement("p")
-paragraph.style.color = "red"
-paragraph.textContent = "Hey I'm red!"
+const paragraph = document.createElement("p");
+paragraph.style.color = "red";
+paragraph.textContent = "Hey I'm red!";
 
-document.body.appendChild(paragraph)
+document.body.appendChild(paragraph);
 
-const headingThree = document.createElement("h3")
-headingThree.style.color = "blue"
-headingThree.textContent = "I'm a blue h3!"
+const headingThree = document.createElement("h3");
+headingThree.style.color = "blue";
+headingThree.textContent = "I'm a blue h3!";
 
-document.body.appendChild(headingThree)
+document.body.appendChild(headingThree);
 
 // creating div
-const element = document.createElement("div")
-element.style.borderStyle = "solid"
-element.style.borderColor = "black"
-element.style.backgroundColor = "pink"
+const element = document.createElement("div");
+element.style.borderStyle = "solid";
+element.style.borderColor = "black";
+element.style.backgroundColor = "pink";
 
-document.body.appendChild(element)
+document.body.appendChild(element);
 
-const headingOne = document.createElement("h1")
-headingOne.textContent = "I'm in a div"
-const anotherPara = document.createElement("p")
-anotherPara.textContent = "ME TOO!"
-document.querySelector("div").appendChild(headingOne)
-document.querySelector("div").appendChild(anotherPara)
+const headingOne = document.createElement("h1");
+headingOne.textContent = "I'm in a div";
+const anotherPara = document.createElement("p");
+anotherPara.textContent = "ME TOO!";
+document.querySelector("div").appendChild(headingOne);
+document.querySelector("div").appendChild(anotherPara);
+
+// events
+
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", function (e) {
+  console.log(e);
+  e.target.style.background = "blue";
+});
+
+const buttons = document.querySelectorAll("button");
+buttons.forEach((button) => {
+  button.addEventListener("click", () => {
+    console.log(button.id);
+  });
+});
